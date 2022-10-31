56cfbe90ca35356956ae29215f51d0c814b222a4
Tim McIver <tim@timmciver.com>
2022-10-30 23:25:02 -0400

    Add EXIF metadata to file URI, not upload URI. Add debugging echos.

--- upload-file.sh
+++ upload-file.sh
@@ -19,6 +19,8 @@ print_usage()
     printf "  --file-content-type MEDIA_TYPE       Media type of the file (optional)\n"
 }
 
+DEBUG=0
+
 hash curl 2>/dev/null || { echo >&2 "curl not on \$PATH. Aborting."; exit 1; }
 
 args=()
@@ -111,7 +113,7 @@ file_container="${base}files/"
 pushd "$SCRIPT_ROOT"
 
 # upload the file and capture the document URI
-file_doc=$(./imports/create-file.sh \
+file_uri=$(./imports/create-file.sh \
     -b "$base" \
     -f "$cert_pem_file" \
     -p "$cert_password" \
@@ -119,25 +121,35 @@ file_doc=$(./imports/create-file.sh \
     --file "${file}" \
     --file-content-type "${file_content_type}")
 
+if [ $DEBUG -ne 0 ]; then
+    echo "file_uri:"
+    echo $file_uri
+fi
+
 # fetch the triples for the document
 file_ntriples=$(./get-document.sh \
     -f "$cert_pem_file" \
     -p "$cert_password" \
     --accept 'application/n-triples' \
-    "$file_doc")
+    "$file_uri")
+
+if [ $DEBUG -ne 0 ]; then
+    echo "file_ntriples:"
+    echo $file_ntriples
+fi
 
 popd
-
-# extract the file URI
-file_uri=$(echo "$file_ntriples" | grep '<http://xmlns.com/foaf/0.1/primaryTopic>' | cut -d " " -f 3 | cut -d "<" -f 2 | cut -d ">" -f 1)
 
 # extract EXIF data from image file as RDF-XML
 exif_rdf_xml=$(perl exif2rdf.pl "${file}")
 
-esc_file_uri="${file_uri@Q}"
-
 # replace file path with file URI in RDF-XML
 exif_rdf_xml=$(echo "$exif_rdf_xml" | sed -e 's/^<foaf:Image rdf:about.*/<foaf:Image rdf:about="'"${file_uri//\//\\/}"'">/')
+
+if [ $DEBUG -ne 0 ]; then
+    echo "exif:"
+    echo $exif_rdf_xml
+fi
 
 # update the RDF for the file URI
 pushd "$SCRIPT_ROOT"
@@ -146,6 +158,6 @@ echo "$exif_rdf_xml" | ./create-document.sh \
                            -f "$cert_pem_file" \
                            -p "$cert_password" \
                            -t "application/rdf+xml" \
-                           "$file_doc"
+                           "$file_uri"
 
 popd
